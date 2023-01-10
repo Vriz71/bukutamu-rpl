6c1687d66eb2fed3124b3c76845b1d4a206cbf3b
Vriz71 <shawdikap775@gmail.com>
2023-01-10 21:57:09 +0700

    menyelesaikan fe 3 halaman

---
+++ fe_bukutamu/pages/guestInput.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Head from 'next/head'
+import Navbar from './components/Navbar'
+
+export default function GuestInput() {
+    return (
+        <>
+            <Head>
+                <title>Guest Book | Input Data</title>
+            </Head>
+
+            <div className='flex h-full'>
+                <div className="flex flex-col w-80 px-4 py-8 border-r">
+                    <Navbar />
+                </div>
+                <div className='w-full p-4 m-12 bg-slate-200 mt-28'></div>
+            </div>
+        </>
+    )
+}
