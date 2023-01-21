824816ff249431d43c946d41790deb68d779a242
Vriz71 <90502533+Vriz71@users.noreply.github.com>
2023-01-21 20:35:00 +0700

    update tester fetch api

--- fe_bukutamu/pages/fetchApi.js
+++ fe_bukutamu/pages/fetchApi.js
@@ -1,43 +1,33 @@
-import React, { useState, useEffect } from 'react'
-
-const Blog = () => {
-    const [posts, setPosts] = useState([]);
-
-    const fetchData = () => {
-        fetch("https://jsonplaceholder.typicode.com/albums").then((response) => {
-            return response.json();
-        }).then((data) => {
-            setPosts(data)
-        })
-    }
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-
+export default function Fetch({ externalPostData }) {
     return (
-        <div className='flex-col my-10'>
-            <h1 className='text-4xl font-bold text-slate-700 text-center my-8'>
-                My <span className='text-green-700'>Albums</span>
-            </h1>
+        <div>
+            <main>
+                <h1>Data</h1>
 
-            {
-                posts.length > 0 && (
-                    <div className='flex justify-center items-center'>
-                        <div className='grid grid-cols-1 md:grid-cols-4 gap-5'>
-                            {
-                                posts.map((post) => (
-                                    <div className='md:mx-0 mx-5 w-72 h-48 rounded-lg cursor-pointer bg-blue-600 shadow-lg'>
-                                        <h3 key={post.id}>{post.title}</h3>
-                                    </div>
-                                ))
-                            }
-                        </div>
-                    </div>
-                )
-            }
+                <div >
+                    {externalPostData.map((data) => {
+                        return (
+                            <div key={data.id}>
+                                {/* disini ganti jadi data.message aja */}
+                                <h3>{data.url}</h3>
+                            </div>
+                        );
+                    })}
+                </div>
+            </main>
         </div>
-    )
+    );
 }
 
-export default Blog
+export async function getStaticProps() {
+    // nanti url nya ganti, yang ini di komentarin aja
+    const response = await fetch("https://jsonplaceholder.typicode.com/photos");
+
+    const data = await response.json();
+
+    return {
+        props: {
+            externalPostData: data,
+        },
+    };
+}
